0194d105791a5fa85cdf9b2b9bf17c31ce49dbe4
oreoeo <ord4@zips.uakron.edu>
2021-03-20 16:44:13 -0400

    Update init, no tfs bindings.

--- init.el
+++ init.el
@@ -18,6 +18,18 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
+;; Highlight current line
+(global-hl-line-mode +1)
+
+;; Disable auto saving/restore files
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+
+;; Showing matching paren mode
+(define-globalized-minor-mode my-global-paren-mode show-paren-mode
+  (lambda () (show-paren-mode 1)))
+(my-global-paren-mode 1)
+
 ;; Multiple cursors
 (require 'multiple-cursors)
 (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
@@ -25,12 +37,16 @@
 ;; Set the warning bell to visual
 (setq visible-bell 1)
 
+;; Ace Window
+(global-set-key (kbd "M-o") 'ace-window)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (multiple-cursors markdown-mode spacemacs-theme))))
+ '(package-selected-packages
+   '(ace-window multiple-cursors markdown-mode spacemacs-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
